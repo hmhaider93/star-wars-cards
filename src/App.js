f2ed893f6944297a70bd87d623106736b1c49920
Haider Mushtaq <hmhaider93@gmail.com>
2022-04-07 22:02:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
+	const [movies, setMovies] = useState([]);
 
-  const [movies, setMovies] = useState([]);
+	async function fetchMoviesHandler() {
+		const response = await fetch("https://swapi.dev/api/films/");
+		const data = await response.json();
+		const transformedMovies = data.results.map((result) => {
+			return {
+				id: result.episode_id,
+				title: result.title,
+				openingText: result.opening_crawl,
+				releaseDate: result.release_date,
+			};
+		});
+		setMovies(transformedMovies);
+	}
 
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films/').then((response)=>{
-      return response.json();
-    }).then(data=>{
-      const transformedMovies = data.results.map(result =>{
-        return {
-          id: result.episode_id,
-          title: result.title,
-          openingText: result.opening_crawl,
-          releaseDate: result.release_date
-        }
-      })
-      setMovies(transformedMovies);
-    });
-  }
-
-  return (
-    <React.Fragment>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
